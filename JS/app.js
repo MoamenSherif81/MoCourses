865c691e32110e98d46b7b7d14da047e02d81cf1
MoamenSherif <moamenyt@gmail.com>
2023-01-16 00:58:14 +0200

    .

--- JS/app.js
+++ JS/app.js
@@ -15,8 +15,26 @@ $(document).ready(function(){
     $('nav').toggleClass('nav-down', $(this).scrollTop() > $('nav').height())
   });
   $('.side-nav-btn').click(() => {
-    $('.nav-items-cont').toggleClass('expand');
+    //$('.nav-items-cont').toggleClass('expand');
+    if($('.nav-items-cont').attr('expanded') == 'false'){
+      $('.nav-items-cont').height($('.nav-items-cont').prop("scrollHeight") + 'px');
+      $('.nav-items-cont').attr('expanded', 'true');
+    } else {
+      $('.nav-items-cont').height('0');
+      $('.nav-items-cont').attr('expanded', 'false');
+    }
   })
+  $(window).on('resize', function(event){
+    if($(window).width() > 768){
+      $('.nav-items-cont').height('auto');
+    } else {
+      $('.nav-items-cont').height(0);
+      if($('.nav-items-cont').attr('expanded') == 'true'){
+        console.log('Here');
+        $('.nav-items-cont').height($('.nav-items-cont').prop("scrollHeight") + 'px');
+      }
+    }
+  });
   //nav bar end
 
   //Main slider start
