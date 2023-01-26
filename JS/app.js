c301189039d0323305338d79578ee270285a6c83
MoamenSherif <moamenyt@gmail.com>
2023-01-26 11:41:27 +0200

    add task page

--- JS/app.js
+++ JS/app.js
@@ -3,37 +3,37 @@ $(document).ready(() => {
   $(document).scroll(() => {
     $('nav').toggleClass('nav-down', $(this).scrollTop() > $('nav').height())
   });
-  $('.side-nav-btn').click(swapNav)
+  /* Event listner for the navbar button */
+  $('.side-nav-btn-cont').click(swapNav)
+  /* Event listner for the window resize to adjust the nav layout */
   $(window).on('resize', () => {
-    if($(window).width() > 768){
+    if($(window).width() > 768)
       $('.nav-items-cont').height('auto');
-    } else {
+    else {
       $('.nav-items-cont').height(0);
       if($('.side-nav-btn').hasClass('active')){
         swapNav();
       }
     }
   });
-  //function to swap the condition of the navbar
-  function swapNav(){
-    if(!$('.side-nav-btn').hasClass('active')){
-      $('.nav-items-cont').height($('.nav-items-cont').prop("scrollHeight") + 'px');
-    } else {
-      $('.nav-items-cont').height('0');
-    }
-    $('.side-nav-btn').toggleClass('active');
-    $('body').toggleClass('nav-expanded');
-  }
   //Close navbar if clicked outside
   $(document).click((e) => {
     if(!($(e.target).is('.nav-content')) && !$(e.target).parents().is('.nav-content')){
       if($('.side-nav-btn').hasClass('active')){
-        $('.side-nav-btn').toggleClass('active');
-        $('body').toggleClass('nav-expanded');
-        $('.nav-items-cont').height('0');
+        swapNav();
       }
     }
   })
+  //function to swap the condition of the navbar
+  function swapNav(){
+    if($('.side-nav-btn').hasClass('active')){
+      $('.nav-items-cont').height('0');
+    } else {
+      $('.nav-items-cont').height($('.nav-items-cont').prop("scrollHeight") + 'px');
+    }
+    $('.side-nav-btn').toggleClass('active');
+    $('body').toggleClass('nav-expanded');
+  }
   //nav bar end
   const date = new Date();
   const dateCont = document.getElementById('date');
---
+++ JS/tasks.js
@@ -0,0 +1,28 @@
+$('.course-details').click((e) => {
+  let id = $(e.target).closest('.course-details').attr('targetid');
+  let tasks = $('.tasks');
+  for(let i = 0; i < tasks.length; i++){
+    if($(tasks[i]).attr('id') == id){
+      if($(tasks[i]).attr('expanded') == 'false'){
+        showTask(tasks[i], e);
+      } else {
+        hideTask(tasks[i]);
+      }
+    }else{
+      hideTask(tasks[i]);
+    }
+  }
+});
+
+function showTask(task, e){
+  $(task).show();
+  let height = $(task).prop('scrollHeight');
+  $(task).css({'height': height + 'px', 'padding':'10px 10px'});
+  $(task).attr('expanded', 'true');
+  $(e.target).closest('.course-details').find('.task-expand').css('transform', 'rotate(180deg)');
+}
+function hideTask(task){
+  $(task).css({'height': 0, 'padding':'0px 10px'});
+  $(task).attr('expanded', 'false');
+  $(task).closest('.course').find('.task-expand').css('transform', 'rotate(0deg)');
+}
