018c9a21c865f2c60b94a31c55033cb54db81eb8
MoamenSherif <moamenyt@gmail.com>
2023-01-31 21:11:35 +0200

    fix some bugs

--- JS/app.js
+++ JS/app.js
@@ -1,8 +1,11 @@
+let profileCont = $('.profile-cont');
+let sideNavBtn = $('.side-nav-btn');
+let sideNavBCont = $('.nav-items-cont');
+
 $(document).ready(() => {
   //nav bar start
-  $(document).scroll(() => {
-    $('nav').toggleClass('nav-down', $(this).scrollTop() > $('nav').height())
-  });
+  checkScroll();
+  $(document).scroll(checkScroll);
   /* Event listner for the navbar button */
   $('.side-nav-btn-cont').click(swapNav)
   $('.profile-icon').click(swapProfile)
@@ -10,10 +13,10 @@ $(document).ready(() => {
   /* Event listner for the window resize to adjust the nav layout */
   $(window).on('resize', () => {
     if($(window).width() > 753)
-      $('.nav-items-cont').height('auto');
+      sideNavBCont.height('auto');
     else {
-      $('.nav-items-cont').height(0);
-      if($('.side-nav-btn').hasClass('active')){
+      sideNavBCont.height(0);
+      if(sideNavBtn.hasClass('active')){
         swapNav();
       }
     }
@@ -21,8 +24,13 @@ $(document).ready(() => {
   //Close navbar if clicked outside
   $(document).click((e) => {
     if(!($(e.target).is('.nav-content')) && !$(e.target).parents().is('.nav-content')){
-      if($('.side-nav-btn').hasClass('active')){
+      if(sideNavBtn.hasClass('active')){
         swapNav();
+      }
+    }
+    if(!($(e.target).is('.profile')) && !$(e.target).parents().is('.profile')){
+      if(profileCont.hasClass('active')){
+        swapProfile();
       }
     }
   })
@@ -30,8 +38,7 @@ $(document).ready(() => {
   
   //nav bar end
   const date = new Date();
-  const dateCont = document.getElementById('date');
-  dateCont.innerHTML = date.getFullYear();
+  $('#date').text(date.getFullYear())
 });
 
 /* Check if the logged in user is a student or not */
@@ -40,8 +47,12 @@ if(localStorage.getItem('role') != 'student'){
 }
 
 /* Check if the user is logged in or not to change the drop down content */
-if(localStorage.getItem('email')) $('.not-logged-in').hide();
-else $('.logged-in').hide();
+if(localStorage.getItem('email')) {
+  $('.not-logged-in').hide();
+  $('.profile-name').text(localStorage.getItem('name'));
+}
+else 
+$('.logged-in').hide();
 
 /* Function to logout */
 function logout(){
@@ -53,24 +64,28 @@ function logout(){
 
 /* Function to swap the Nav dropdown menu */
 function swapNav(){
-  if($('.side-nav-btn').hasClass('active')){
-    $('.nav-items-cont').height('0');
+  if(sideNavBtn.hasClass('active')){
+    sideNavBCont.height('0');
   } else {
-    $('.nav-items-cont').height($('.nav-items-cont').prop("scrollHeight") + 'px');
+    sideNavBCont.height(sideNavBCont.prop("scrollHeight") + 'px');
   }
-  $('.side-nav-btn').toggleClass('active');
+  sideNavBtn.toggleClass('active');
   $('body').toggleClass('nav-expanded');
 }
 
 /* Function to swap the profile dropdown menu */
 function swapProfile(){
-  if($('.profile-cont').hasClass('active')){
-    $('.profile-cont').css({'height':'0'});
+  if(profileCont.hasClass('active')){
+    profileCont.css({'height':'0'});
   } else {
-    console.log($('.profile-list').prop("scrollHeight"));
-    $('.profile-cont').css({'height':$('.profile-list').prop("scrollHeight") + 'px'});
+    profileCont.css({'height':$('.profile-list').prop("scrollHeight") + 'px'});
   }
-  $('.profile-cont').toggleClass('active');
+  profileCont.toggleClass('active');
+}
+
+/* Function to change the background color of the navbar based on the scroll */
+function checkScroll(){
+  $('nav').toggleClass('nav-down', $(this).scrollTop() > $('nav').height())
 }
 
 /* Function that go to the single course page */
